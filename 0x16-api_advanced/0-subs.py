0597ae413dc8e8a147728de55c23a408819e5aa2
Naphtal USABYUWERA <usanaphtal112@gmail.com>
2024-05-07 23:05:52 -0700

    fixed the sub file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 """use Reddit API and returnsthe number of subscribers"""
-import requests
 
+import requests
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "Mozilla/5.0"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Mozilla/5.0"}
+
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        data = response.json()
+        subscribers = data["data"]["subscribers"]
+        return subscribers
+    except requests.RequestException as e:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    except KeyError:
+        return 0
+
