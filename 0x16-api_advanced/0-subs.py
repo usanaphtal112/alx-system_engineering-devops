346fe8eacef1df96660b85714ef5142a72e67b0c
Naphtal USABYUWERA <usanaphtal112@gmail.com>
2024-05-07 23:08:57 -0700

    fixed the sub file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-"""use Reddit API and returnsthe number of subscribers"""
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-
-    try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
         data = response.json()
-        subscribers = data["data"]["subscribers"]
+        subscribers = data['data']['subscribers']
         return subscribers
-    except requests.RequestException as e:
+    else:
         return 0
-    except KeyError:
-        return 0
-
